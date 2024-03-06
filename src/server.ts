c47c8cd85bab4e6a8489df675710a661f476b54c
thadeu <guilherme.thadeu@qodeless.io>
2024-03-05 21:01:01 -0300

    feat: install zod

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,24 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação  de teste',
+      amount: 1000,
+    })
+    .returning('*')
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!!')
