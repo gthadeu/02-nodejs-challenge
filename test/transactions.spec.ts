6c22b7005b4e6fdf34380428690b393d05906068
thadeu <guilherme.thadeu@qodeless.io>
2024-03-07 20:18:28 -0300

    feat: list transactions test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { test, beforeAll, afterAll, describe } from 'vitest'
+import { test, beforeAll, afterAll, describe, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -20,5 +20,29 @@ describe('Transactions routes', () => {
         type: 'debit',
       })
       .expect(201)
+  })
+
+  test('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Feather',
+        amount: 100,
+        type: 'debit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'Feather',
+        amount: -100,
+      }),
+    ])
   })
 })
